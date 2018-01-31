461dfda12cd184326a8a5066aee4cb3b0a593ec3
Jacob Voyles <jtvkw2@gmail.com>
2018-01-31 12:13:31 -0800

    Improved code readability and font

--- MeMe/ViewController.swift
+++ MeMe/ViewController.swift
@@ -16,22 +16,19 @@ struct Meme {
     var memedImage : UIImage
 }
 
+
+
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
 UINavigationControllerDelegate {
 
-
     @IBOutlet weak var ImagePickerView: UIImageView!
     @IBOutlet weak var shareButton: UIBarButtonItem!
-    
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var topText: UITextField!
     @IBOutlet weak var bottomText: UITextField!
-    
     @IBOutlet weak var topToolBar: UIToolbar!
-    
     @IBOutlet weak var bottomToolBar: UIToolbar!
-  
-    
+    @IBOutlet weak var CancalButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,31 +38,32 @@ UINavigationControllerDelegate {
         
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)
         
-        let memeTextAttributes : [String : Any] = [
+        let attributes : [String : Any] = [
             NSAttributedStringKey.foregroundColor.rawValue: UIColor.white,
-            NSAttributedStringKey.font.rawValue: UIFont(name: "Papya", size:40) ??  UIFont.systemFont(ofSize: 40)
+            NSAttributedStringKey.font.rawValue: UIFont(name: "ComicSansMS", size: 40) ?? UIFont.systemFont(ofSize: 30)
             ]
         
-        topText.defaultTextAttributes = memeTextAttributes
+        topText.defaultTextAttributes = attributes
         topText.textAlignment = NSTextAlignment.center
-        topText.text = "TOP"
-        topText.isHidden = false;
+        topText.text = "Top Text"
         
-        bottomText.defaultTextAttributes = memeTextAttributes
+        bottomText.defaultTextAttributes = attributes
         bottomText.textAlignment = NSTextAlignment.center
-        bottomText.text = "BOTTOM"
-        bottomText.isHidden = false;
-        // Do any additional setup after loading the view, typically from a nib.
+        bottomText.text = "Bottom Text"
         
         subscribeToKeyboardNotifications()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        
         super.viewWillDisappear(animated)
+        
         unsubscribeFromKeyboardNotifications()
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
     @IBAction func pickAnImageCamera(sender: AnyObject) {
         
         let pickerController = UIImagePickerController()
@@ -74,7 +72,6 @@ UINavigationControllerDelegate {
         present(pickerController, animated:true, completion:nil)
     }
     
-    //execute this when user selects album icon
     @IBAction func pickAnImageAlbum(sender: AnyObject) {
         
         let pickerController = UIImagePickerController()
@@ -83,7 +80,32 @@ UINavigationControllerDelegate {
         present(pickerController, animated:true, completion:nil)
     }
     
-    //executed when user selects image from image picker
+    @IBAction func takeAction(sender: AnyObject) {
+        
+        let memedImage = generateMemedImage()
+        
+        print("I have a meme!")
+        
+        let activityViewController = UIActivityViewController(activityItems: [memedImage],applicationActivities: nil)
+        
+        present(activityViewController, animated: true)
+        activityViewController.completionWithItemsHandler = {
+            (activity, completed, items, error) in
+            if (completed)
+            {
+                self.save(memedImage: memedImage)
+            }
+        }
+    }
+    
+    @IBAction func Cancel(_ sender: Any) {
+        ImagePickerView.image = nil
+        topText.text = "Top Text"
+        bottomText.text = "Bottom Text"
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     func imagePickerController(_ picker: UIImagePickerController,
                                         didFinishPickingMediaWithInfo info: [String : Any]) {
         
@@ -100,13 +122,9 @@ UINavigationControllerDelegate {
     }
     
     func generateMemedImage() -> UIImage {
-        
-        //we want to hide the toolbars, so that they are not
-        //part of the meme
         topToolBar.isHidden = true
         bottomToolBar.isHidden = true
         
-        //create the image
         UIGraphicsBeginImageContext(view.frame.size)
         view.drawHierarchy(in: view.frame,
                                      afterScreenUpdates: true)
@@ -114,7 +132,6 @@ UINavigationControllerDelegate {
             UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
         
-        //once the image is created, the toolbars can be redisplayed
         topToolBar.isHidden = false
         bottomToolBar.isHidden = false
         
@@ -127,29 +144,6 @@ UINavigationControllerDelegate {
         
         (UIApplication.shared.delegate as! AppDelegate).memes.append(meme)
     }
-    
-    
-    //this function is executed when the Action button is selected
-    //the meme is generated and the Activity View Controller is displayed
-    @IBAction func takeAction(sender: AnyObject) {
-        
-        //memed image, with image and text is generated
-        let memedImage = generateMemedImage()
-        print("I have a meme")
-        //create the Activity View Controller
-        let activityViewController = UIActivityViewController(activityItems: [memedImage],applicationActivities: nil)
-        
-        present(activityViewController, animated: true)
-        
-        //this sets up the save of the meme, which will be done when the Activity View Controller is completed
-        activityViewController.completionWithItemsHandler = {
-            (activity, completed, items, error) in
-            if (completed)
-            {
-                self.save(memedImage: memedImage)
-            }
-        }
-}
     
     func subscribeToKeyboardNotifications() {
         
@@ -170,8 +164,13 @@ UINavigationControllerDelegate {
         NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
     }
     
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
     @objc func keyboardWillShow(_ notification:Notification) {
-        
         if (bottomText.isFirstResponder)
         {
             view.frame.origin.y -= getKeyboardHeight(notification)
@@ -179,23 +178,9 @@ UINavigationControllerDelegate {
     }
     
     @objc func keyboardWillHide(_ notification: NSNotification) {
-        print(bottomText.isFirstResponder)
         if (bottomText.isFirstResponder)
         {
             view.frame.origin.y += getKeyboardHeight(notification as Notification)
         }
     }
-    
-    
-    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
-        
-        let userInfo = notification.userInfo
-        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
-        return keyboardSize.cgRectValue.height
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        view.endEditing(true)
-    }
-
 }
